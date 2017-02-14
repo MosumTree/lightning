ed702c3c44dddad95d1e4462dc343c1a67a0ce59
MosumTree <mosumtree@gmail.com>
2017-02-14 21:45:22 +0800

    searchbar commit

--- app/src/main/java/com/example/mosum/lightning/MainActivity.java
+++ app/src/main/java/com/example/mosum/lightning/MainActivity.java
@@ -1,14 +1,20 @@
 package com.example.mosum.lightning;
 
+import android.content.Intent;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Gravity;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.ListView;
+
+import com.miguelcatalan.materialsearchview.MaterialSearchView;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -21,21 +27,23 @@ import static android.R.id.list;
 public class MainActivity extends FragmentActivity {
     private DrawerLayout drawerLayout;
     private ImageView leftMenu;
-    private ListView listView;
+    private ListView leftlistView;
     private FragmentManager fm;
     private List<ContentModel> list;
     private ContentAdapter adapter;
+
+    private Button searchbt;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         leftMenu = (ImageView) findViewById(R.id.leftmenu);
         drawerLayout = (DrawerLayout) findViewById(R.id.drawerlayout);
-        listView = (ListView) findViewById(R.id.left_listview);
+        leftlistView = (ListView) findViewById(R.id.left_listview);
         fm = getSupportFragmentManager();
         initLeftmenu();
         adapter = new ContentAdapter(this, list);
-        listView.setAdapter(adapter);
+        leftlistView.setAdapter(adapter);
         leftMenu.setOnClickListener(new View.OnClickListener() {
 
             @Override
@@ -43,7 +51,20 @@ public class MainActivity extends FragmentActivity {
                 drawerLayout.openDrawer(Gravity.LEFT);
             }
         });
+
+        searchbt = (Button) findViewById(R.id.search_bt);
+        searchbt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, SearchActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
+
+
+
 
     //初始化侧边栏菜单
     private void initLeftmenu() {
@@ -55,4 +76,5 @@ public class MainActivity extends FragmentActivity {
         list.add(new ContentModel(R.drawable.slidemenu_compasmode, "罗盘模式", 5));
         list.add(new ContentModel(R.drawable.slidemenu_buslocation, "校车位置", 6));
     }
+    //
 }
